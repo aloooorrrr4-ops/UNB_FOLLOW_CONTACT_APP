162b7954783944faf6126cd20f2a0f5966cb5619
aloooorrrr4-ops <aloooorrrr4@gmail.com>
2026-09-23 06:24:37 +0300

    feat: use precision pointer for magic and color selection

--- app/src/main/java/com/unb/imageeditor/EditorCanvasView.java
+++ app/src/main/java/com/unb/imageeditor/EditorCanvasView.java
@@ -30,13 +30,13 @@ public class EditorCanvasView extends View {
     private static final Set<String> STROKE_MODES = new HashSet<>(Arrays.asList(
             "brush", "pencil", "eraser", "clone", "heal", "smudge",
             "dodge_burn", "gradient", "select", "free_select", "crop", "color_picker",
-            "text_select", "fill"
+            "text_select", "fill", "fuzzy_select", "color_select"
     ));
 
     private static final Set<String> DETACHED_POINTER_MODES = new HashSet<>(Arrays.asList(
             "brush", "pencil", "eraser", "clone", "heal", "smudge",
             "dodge_burn", "gradient", "select", "free_select", "crop", "color_picker",
-            "text_select", "fill"
+            "text_select", "fill", "fuzzy_select", "color_select"
     ));
 
     private final Paint checkerA = new Paint(Paint.ANTI_ALIAS_FLAG);
